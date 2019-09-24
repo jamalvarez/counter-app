99c67188bf0ad5c658188f21ce14bea9f6f458a3
Jesús Marín <jmarin@falabella.cl>
2019-09-24 14:19:20 -0300

    look at that, we can filter based on an interval

--- src/components/filters/index.js
+++ src/components/filters/index.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from "react";
 
-
-function Filters(){
-    return(
-        <div id="filters"></div>
-    )
+function Filters() {
+  const [maxFilter, setMax] = useState({ currentValue: "100", topValue: "100" });
+  const [minFilter, setMin] = useState({ currentValue: "1", bottomValue: "1" });
+  return (
+    <div id="filters">
+      <input
+        class="min"
+        name="range_1"
+        type="range"
+        min={minFilter.bottomValue}
+        max={maxFilter.topValue}
+        value={minFilter.currentValue}
+        onChange={e => {
+          if (Number(e.target.value) < Number(maxFilter.currentValue)) {
+            setMin({ currentValue: e.target.value, bottomValue: "1" });
+          }
+        }}
+      />
+      <input
+        class="max"
+        name="range_1"
+        type="range"
+        min={minFilter.bottomValue}
+        max={maxFilter.topValue}
+        value={maxFilter.currentValue}
+        onInput={e => {
+          if (Number(e.target.value) > Number(minFilter.currentValue)) {
+            setMax({ currentValue: e.target.value, topValue: "100" });
+          }
+        }}
+      />
+    </div>
+  );
 }
-
 
 export default Filters;
