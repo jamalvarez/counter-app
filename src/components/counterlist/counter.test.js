9f7d53174a222d5b7a0be1507dd486d016432175
Jesús Marín <jmarin@falabella.cl>
2019-09-25 22:20:40 -0300

    added counter tests

--- src/components/counterlist/counter.test.js
+++ src/components/counterlist/counter.test.js
@@ -3,13 +3,39 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 const onClick = jest.fn();
-const wrapper = shallow(<Counter {...{id:'123', title:'title', count:'2', onAction, onFailure}} />);
-
-it('Renders a div with id "counter-adder"', () => {
-  expect(wrapper.find('#counter-adder').length).toBe(1);
+let wrapper;
+let actions;
+beforeEach(() => {
+  actions = {remove:jest.fn(), increase:jest.fn(), decrease:jest.fn()};
+  wrapper = shallow(<Counter {...{id:'123', title:'title', count:2, actions}} />);
 })
 
-it('Calls the raisepopup function prop on click', () => {
-  wrapper.find('#counter-adder').simulate('click');
-  expect((onClick)).toHaveBeenCalled();
+it('Renders divs with the classes remove, title, decrease, increase and countnumber', () => {
+  expect(wrapper.find('.title').length).toBe(1);
+  expect(wrapper.find('.remove').length).toBe(1);
+  expect(wrapper.find('.decrease').length).toBe(1);
+  expect(wrapper.find('.increase').length).toBe(1);
+  expect(wrapper.find('.countnumber').length).toBe(1);
+
+})
+
+it('Calls the decrease action when .decrease is clicked', () => {
+  wrapper.find('.decrease').simulate('click');
+  expect((actions.decrease)).toHaveBeenCalled();
+})
+
+it('Doesn\'t call the decrease action when the count is 0', () => {
+  wrapper = shallow(<Counter {...{id:'123', title:'title', count:0, actions}} />);
+  wrapper.find('.decrease').simulate('click');
+  expect((actions.decrease)).not.toHaveBeenCalled();
+})
+
+it('Calls the increase action when .decrease is clicked', () => {
+  wrapper.find('.increase').simulate('click');
+  expect((actions.increase)).toHaveBeenCalled();
+})
+
+it('Calls the remove action when .decrease is clicked', () => {
+  wrapper.find('.remove').simulate('click');
+  expect((actions.remove)).toHaveBeenCalled();
 })
